75a67cba7fa3dc24b1ef718ab88aad6c2805e5a3
joelmartindev <joel.martin98@gmail.com>
2023-05-20 16:58:56 +0300

    Single station view shows total journeys

--- frontend/src/components/SingleStation.jsx
+++ frontend/src/components/SingleStation.jsx
@@ -1,10 +1,21 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import StationsContext from "./StationsContext"
+import db from "../services/stationDB"
 
 const SingleStation = () => {
     const id = Number(useParams('id').id)
     const stations = useContext(StationsContext)
+    const [totalJourneys, setTotalJourneys] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await db.getTotalJourneys(id)
+            setTotalJourneys(res)
+        }
+
+        if (id !== null) fetchData()
+    },[])
 
     if (stations === null) return <>Fetching data...</>
 
@@ -12,12 +23,17 @@ const SingleStation = () => {
  
     if (station === undefined) return <>Station not found</>
 
+
     return (
         <>
             <p>{station.name_fi}</p>
             <p>{station.address_fi}</p>
-            <p>TODO Total number of journeys starting from the station</p>
-            <p>TODO Total number of journeys ending at the station</p>
+            {totalJourneys === null 
+            ? <p>Loading Total number of journeys starting from the station</p>
+            : <p>Total departures {totalJourneys.departuresCount}</p>}
+            {totalJourneys === null 
+            ? <p>Loading Total number of journeys ending at the station</p>
+            : <p>Total returns {totalJourneys.returnsCount}</p>}
         </>
     )
 }
--- frontend/src/services/stationDB.js
+++ frontend/src/services/stationDB.js
@@ -1,9 +1,16 @@
 const getAll = async () => {
-    const response = await fetch('http://localhost:3000/stations');
+    const response = await fetch('http://localhost:3000/api/stations');
+    const jsonData = await response.json()
+    return jsonData
+}
+
+const getTotalJourneys = async (id) => {
+    const response = await fetch(`http://localhost:3000/api/stations/${id}/journeys`);
     const jsonData = await response.json()
     return jsonData
 }
 
 export default {
-    getAll
+    getAll,
+    getTotalJourneys
 }
