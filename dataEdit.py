ce12748bc5740f830385a6fc41aac8588be8355f
joelmartindev <joel.martin98@gmail.com>
2023-05-20 12:58:13 +0300

    Added more filters in data editing script

--- dataEdit.py
+++ dataEdit.py
@@ -8,7 +8,13 @@ df = pd.read_csv("path/to/file.csv", sep=",")
 
 # Filter out rows with the wrong value
 df = df[df["Covered distance (m)"] >= 10]
-filtered_df = df[df["Duration (sec.)"] >= 10]
+df = df[df["Duration (sec.)"] >= 10]
+df = df[df["Departure station id"] != 997]
+df = df[df["Return station id"] != 997]
+df = df[df["Departure station id"] != 754]
+df = df[df["Return station id"] != 754]
+df = df[df["Departure station id"] != 999]
+filtered_df = df[df["Return station id"] != 999]
 
 # Convert the "Covered distance (m)" column to string data type
 filtered_df["Covered distance (m)"] = filtered_df["Covered distance (m)"].astype(str).apply(lambda x: x.rstrip('0').rstrip('.'))
