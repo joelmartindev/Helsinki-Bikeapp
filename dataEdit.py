68d33073a554842522f248ccd1c127bd8e28d38a
joelmartindev <joel.martin98@gmail.com>
2023-05-23 17:17:29 +0300

    Added removing duplicates to data editing script

--- dataEdit.py
+++ dataEdit.py
@@ -1,12 +1,14 @@
-
-# Run this script to remove useless data
+# Run this script for each file to remove useless data
 
 # Read the CSV file
 import pandas as pd
 
 df = pd.read_csv("path/to/file.csv", sep=",")
 
-# Filter out rows with the wrong value
+# Drop duplicate data
+df.drop_duplicates(subset=None, inplace=True)
+
+# Filter out rows with the wrong value or stations that can't be found in the list of stations
 df = df[df["Covered distance (m)"] >= 10]
 df = df[df["Duration (sec.)"] >= 10]
 df = df[df["Departure station id"] != 997]
@@ -17,7 +19,11 @@ df = df[df["Departure station id"] != 999]
 filtered_df = df[df["Return station id"] != 999]
 
 # Convert the "Covered distance (m)" column to string data type
-filtered_df["Covered distance (m)"] = filtered_df["Covered distance (m)"].astype(str).apply(lambda x: x.rstrip('0').rstrip('.'))
+filtered_df["Covered distance (m)"] = (
+    filtered_df["Covered distance (m)"]
+    .astype(str)
+    .apply(lambda x: x.rstrip("0").rstrip("."))
+)
 
 # Write the filtered data to a new file
 filtered_df.to_csv("path/to/new_file.csv", index=False)
