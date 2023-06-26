11dd67cf92b4de4cac8c3aa2da79f2a678300c53
joelmartindev <joel.martin98@gmail.com>
2023-06-26 21:30:50 +0300

    Fix map marker not showing up in production

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,5 +1,7 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
 
 const Map = ({ coord_y, coord_x, height, name }) => {
   return (
@@ -8,7 +10,16 @@ const Map = ({ coord_y, coord_x, height, name }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[coord_y, coord_x]}>
+      <Marker
+        position={[coord_y, coord_x]}
+        icon={
+          new Icon({
+            iconUrl: markerIcon,
+            iconSize: [25, 41],
+            iconAnchor: [12, 18],
+          })
+        }
+      >
         <Popup>
           <h1>{name}</h1>
         </Popup>
