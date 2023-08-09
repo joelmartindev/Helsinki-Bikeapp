b4c98bf77e87398228d72aae96472f00e81fdc07
joelmartindev <joel.martin98@gmail.com>
2023-08-09 20:48:22 +0300

    Update info on home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,28 +14,38 @@ const Home = () => {
         <Logo className="ml-5 mt-5 h-32 w-32 animate-triangle-long fill-white stroke-white pb-3 sm:ml-10 sm:h-32 sm:w-32 sm:animate-triangle-short sm:pb-0" />
       </div>
       <h2 className="mt-10 bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-rose-500 to-indigo-700 bg-clip-text text-3xl font-extrabold leading-tight text-transparent">
-        Description
+        What is it?
       </h2>
-      <div className="mt-8 max-w-prose text-custom-isabelline">
+      <div className="mt-8 max-w-prose font-mono text-custom-isabelline">
         A showcase of data from the 2021 season of city bikes in Helsinki done
-        as my first portfolio project and as a challenge to deepen my knowledge
-        of web technologies and practices.
+        as my first portfolio project. I created it to deepen my knowledge of
+        web technologies and practices.
       </div>
-      <div className="my-3 max-w-prose text-custom-isabelline">
-        The app is divided into pages to view all data as well as pages for
-        individual journeys and stations.
+      <div className="my-3 max-w-prose font-mono text-custom-isabelline">
+        Start by choosing a tab from the menu above!
       </div>
       <h2 className="mt-10 bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-rose-500 to-indigo-700 bg-clip-text text-3xl font-extrabold leading-tight text-transparent">
-        Features (and upcoming features)
+        Features
       </h2>
-      <ul className="my-5 list-inside list-disc text-custom-isabelline">
-        <li>Display all journeys and stations with pagination</li>
-        <li>Sort data</li>
-        <li>Search based on parameters</li>
-        <li>View data of a specific journey or station on a new page</li>
-        <li>Statistics page for all data</li>
-        <li>Single View: Statistics</li>
-        <li>Single View: Map of station or journey</li>
+      <ul className="my-5 ml-6 list-inside list-disc font-mono text-custom-isabelline">
+        <li className="-indent-6">Explore a map with every station on it</li>
+        <li className="-indent-6">
+          Display all journeys and stations with pagination
+        </li>
+        <li className="-indent-6">
+          Search for stations and journeys based on names and addresses
+        </li>
+        <li className="-indent-6">
+          Click on a journey or station for a detailed view
+        </li>
+        <li className="-indent-6">
+          Single Journey View: Information and a map with departure and return
+          stations
+        </li>
+        <li className="-indent-6">
+          Single Station View: Information, map and statistics
+        </li>
+        <li className="-indent-6">Statistics page for all data</li>
       </ul>
     </div>
   );
